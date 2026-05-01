12ae86a09f29436078a70e0d86be9919d60167f5
ngicks <yknt.bsl@gmail.com>
2026-05-01 16:06:38 +0900

    ✨(cmd): update helper

--- cmd/internal/stdiopipe/stdiopipe.go
+++ cmd/internal/stdiopipe/stdiopipe.go
@@ -8,9 +8,13 @@ import (
 	"sync"
 )
 
-var once sync.Once
+var (
+	onceStdin  sync.Once
+	onceStdout sync.Once
+	onceStderr sync.Once
+)
 
-// Stdin returns an [io.ReadCloser] which is pied to [os.Stdin] through an [io.Pipe].
+// Stdin returns an [io.ReadCloser] which is piped to [os.Stdin] through an [io.Pipe].
 //
 // This is necessary because Read calls on [os.Stdin] cannot be unblocked by closing it.
 //
@@ -18,7 +22,7 @@ var once sync.Once
 func Stdin(ctx context.Context) io.ReadCloser {
 	var pr *io.PipeReader
 	called := false
-	once.Do(func() {
+	onceStdin.Do(func() {
 		called = true
 		var pw *io.PipeWriter
 		pr, pw = io.Pipe()
@@ -32,7 +36,47 @@ func Stdin(ctx context.Context) io.ReadCloser {
 		}()
 	})
 	if !called {
-		panic("stdiopipe: Stdin called more than once")
+		panic("stdiopipe: Stdin is called more than once")
 	}
 	return pr
+}
+
+func stdout(ctx context.Context, label string, out *os.File, once *sync.Once) io.WriteCloser {
+	var pw *io.PipeWriter
+	called := false
+	once.Do(func() {
+		called = true
+		var pr *io.PipeReader
+		pr, pw = io.Pipe()
+		go func() {
+			<-ctx.Done()
+			pw.CloseWithError(ctx.Err())
+		}()
+		go func() {
+			_, err := io.Copy(out, pr)
+			pr.CloseWithError(err)
+		}()
+	})
+	if !called {
+		panic("stdiopipe: " + label + " is called more than once")
+	}
+	return pw
+}
+
+// Stdout returns an [io.WriteCloser] which is piped to [os.Stdout] through an [io.Pipe].
+//
+// This is necessary because Write calls on [os.Stdout] cannot be unblocked by closing it.
+//
+// Only one invocation is allowed per process; a second call will panic.
+func Stdout(ctx context.Context) io.WriteCloser {
+	return stdout(ctx, "Stdout", os.Stdout, &onceStdout)
+}
+
+// Stderr returns an [io.WriteCloser] which is piped to [os.Stderr] through an [io.Pipe].
+//
+// This is necessary because Write calls on [os.Stderr] cannot be unblocked by closing it.
+//
+// Only one invocation is allowed per process; a second call will panic.
+func Stderr(ctx context.Context) io.WriteCloser {
+	return stdout(ctx, "Stderr", os.Stderr, &onceStderr)
 }
