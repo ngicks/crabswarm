17aed94118a60bc806c17afbb949271e7c64c852
ngicks <yknt.bsl@gmail.com>
2026-05-14 03:31:17 +0900

    🔥: remove those ported github.com/ngicks/cmdman

--- pkg/cmdman/store/internal/migrations/migrations.go
+++
@@ -1,9 +0,0 @@
-package migrations
-
-import "database/sql"
-
-// SchemaMigrations maps a target schema version to the function that migrates
-// from the previous version.
-var SchemaMigrations = map[int]func(tx *sql.Tx) error{
-	2: MigrateV1ToV2,
-}
--- pkg/cmdman/store/internal/migrations/v2.go
+++
@@ -1,9 +0,0 @@
-package migrations
-
-import "database/sql"
-
-// MigrateV1ToV2 adds the CreatedAt column to CommandConfig.
-func MigrateV1ToV2(tx *sql.Tx) error {
-	_, err := tx.Exec(`ALTER TABLE CommandConfig ADD COLUMN CreatedAt TEXT NOT NULL DEFAULT ''`)
-	return err
-}
