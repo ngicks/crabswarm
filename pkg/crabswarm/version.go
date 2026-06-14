e588533617bc3fc70d74fc99cd175d8fdf0bbb08
ngicks <yknt.bsl@gmail.com>
2026-06-14 19:46:43 +0900

    👷: start v0.0.8-devel development cycle

--- pkg/crabswarm/version.go
+++ pkg/crabswarm/version.go
@@ -10,4 +10,4 @@ package crabswarm
 //
 // Edit by hand only when the release helper is unavailable (e.g. cherry-pick
 // of a release commit).
-const Version = "v0.0.7"
+const Version = "v0.0.8-devel"
