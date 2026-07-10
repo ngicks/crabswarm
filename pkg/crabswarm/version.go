45082d7b400b76577d6bd8b6d3a01cecf4f7de29
ngicks <yknt.bsl@gmail.com>
2026-07-10 09:27:17 +0900

    🔖: release v0.0.11

--- pkg/crabswarm/version.go
+++ pkg/crabswarm/version.go
@@ -10,4 +10,4 @@ package crabswarm
 //
 // Edit by hand only when the release helper is unavailable (e.g. cherry-pick
 // of a release commit).
-const Version = "v0.0.11-devel"
+const Version = "v0.0.11"
